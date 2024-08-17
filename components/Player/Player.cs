934c90f9708c2d50ec0958710d351a8b469dfd74
fizzydrinks <gabriel@lins.dev>
2024-08-17 10:42:56 -0300

    add constants

--- components/Player/Player.cs
+++ components/Player/Player.cs
@@ -1,33 +1,30 @@
-using Godot;
-using System;
-
 public partial class Player : CharacterBody2D
 {
     public const float Speed = 300.0f;
-    public const float JumpVelocity = -400.0f;
 
     public override void _PhysicsProcess(double delta)
     {
-        Vector2 velocity = Velocity;
+        ApplyGravity(delta);
+        ApplyXMovement();
 
-        // Add the gravity.
+        MoveAndSlide();
+    }
+
+    private void ApplyGravity(double delta)
+    {
         if (!IsOnFloor())
         {
-            velocity += GetGravity() * (float)delta;
+            Velocity += GetGravity() * (float)delta;
         }
+    }
 
-        // Handle Jump.
-        if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+    private void ApplyXMovement()
+    {
+        var velocity = Velocity;
+        var direction = Input.GetAxis(KeyMap.MoveL, KeyMap.MoveR);
+        if (direction != 0f)
         {
-            velocity.Y = JumpVelocity;
-        }
-
-        // Get the input direction and handle the movement/deceleration.
-        // As good practice, you should replace UI actions with custom gameplay actions.
-        Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-        if (direction != Vector2.Zero)
-        {
-            velocity.X = direction.X * Speed;
+            velocity.X = direction * Speed;
         }
         else
         {
@@ -35,6 +32,5 @@ public partial class Player : CharacterBody2D
         }
 
         Velocity = velocity;
-        MoveAndSlide();
     }
 }
---
+++ constants/KeyMap.cs
@@ -0,0 +1,11 @@
+public static class KeyMap
+{
+    public const string MoveR = "MoveR";
+    public const string MoveL = "MoveL";
+    public const string Grab = "Grab";
+    public const string ClimbUp = "ClimbUp";
+    public const string ClimbDown = "ClimbDown";
+    public const string Map = "Map";
+    public const string MapZoomIn = "MapZoomIn";
+    public const string MapZoomOut = "MapZoomOut";
+}
