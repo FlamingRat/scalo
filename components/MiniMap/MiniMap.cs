a94ab44a6eb472eafce494a80404e0eabb69b848
fizzydrinks <gabriel@lins.dev>
2024-08-17 11:45:01 -0300

    map zoom

--- components/MiniMap/MiniMap.cs
+++ components/MiniMap/MiniMap.cs
@@ -1,6 +1,10 @@
 [Tool]
 public partial class MiniMap : CanvasLayer
 {
+    public const float MaxMiniMapScale = 1f;
+    public const float MinMiniMapScale = 0.1f;
+    public const float ZoomSpeed = 0.1f;
+
     [Export]
     public TileMapLayer MainMap;
 
@@ -20,11 +24,8 @@ public partial class MiniMap : CanvasLayer
 
     public override void _Process(double delta)
     {
-        if (Engine.IsEditorHint() && Minimap != null && MainMap != null)
-        {
-            Minimap.TileSet = MainMap.TileSet;
-            Minimap.TileMapData = MainMap.TileMapData;
-        }
+        if (Engine.IsEditorHint()) EditorProcess();
+        else Process(delta);
     }
 
     public override void _Input(InputEvent @event)
@@ -32,6 +33,23 @@ public partial class MiniMap : CanvasLayer
         if (Input.IsActionJustPressed(KeyMap.Map) && !Engine.IsEditorHint())
         {
             Visible = !Visible;
+        }
+    }
+
+    private void Process(double delta)
+    {
+        var zoom = Input.GetAxis(KeyMap.MapZoomOut, KeyMap.MapZoomIn);
+        Minimap.Scale += Vector2.One * zoom * (float)delta;
+
+        Minimap.Scale = Minimap.Scale.Clamp(MinMiniMapScale, MaxMiniMapScale);
+    }
+
+    private void EditorProcess()
+    {
+        if (Minimap != null && MainMap != null)
+        {
+            Minimap.TileSet = MainMap.TileSet;
+            Minimap.TileMapData = MainMap.TileMapData;
         }
     }
 }
