672ed76ed3a24c88ef23f444ba07c71f3f45bc80
Jorge-Edson <jorgeedsonadao@gmail.com>
2024-08-18 14:16:03 -0300

    add Player Marker on MiniMap

--- components/MiniMap/MiniMap.cs
+++ components/MiniMap/MiniMap.cs
@@ -2,64 +2,78 @@ using System.Linq;
 
 public partial class MiniMap : CanvasLayer
 {
-    public const float MaxPlayerScale = 8f;
-    public const float MinPlayerScale = 0.4f;
-    public const float ZoomSpeed = 5f;
-    public const float MiniMapScale = 0.05f;
+	public const float MaxPlayerScale = 8f;
+	public const float MinPlayerScale = 0.4f;
+	public const float ZoomSpeed = 5f;
+	public const float MiniMapScale = 0.05f;
 
-    [Export]
-    public TileMapLayer[] Tilemaps;
+	[Export]
+	public TileMapLayer[] Tilemaps;
 
-    [Export]
-    public Control MinimapContainer;
+	[Export]
+	public Control MinimapContainer;
 
-    [Export]
-    public Node2D Player;
+	[Export]
+	public Node2D Player;
+	
+	[Export]
+	public Sprite2D PlayerMarker;
 
-    public override void _Ready()
-    {
-        foreach (var layer in Tilemaps)
-        {
-            var copy = (TileMapLayer)layer.Duplicate();
-            MinimapContainer.AddChild(copy);
+	public override void _Ready()
+	{
+		foreach (var layer in Tilemaps)
+		{
+			var copy = (TileMapLayer)layer.Duplicate();
+			MinimapContainer.AddChild(copy);
 
-            copy.TileSet = (TileSet)copy.TileSet.Duplicate();
-            copy.TileSet.RemovePhysicsLayer(0);
+			copy.TileSet = (TileSet)copy.TileSet.Duplicate();
+			copy.TileSet.RemovePhysicsLayer(0);
 
-            foreach (var coords in copy.GetUsedCells())
-            {
-                copy.SetCell(coords, 1, new Vector2I(0, 0));
-            }
-        }
+			foreach (var coords in copy.GetUsedCells())
+			{
+				copy.SetCell(coords, 1, new Vector2I(0, 0));
+			}
+		}
 
-        Visible = false;
+		Visible = false;
 
-        UpdateMinimap();
-    }
+		UpdateMinimap();
+	}
 
-    public override void _Process(double delta)
-    {
-        if (!Visible) return;
+	public override void _Process(double delta)
+	{
+		if (!Visible) return;
 
-        UpdateMinimap();
+		UpdateMinimap();
+		UpdatePlayerMarker();
 
-        var zoom = Input.GetAxis(KeyMap.MapZoomOut, KeyMap.MapZoomIn);
-        if (zoom == 0) return;
+		var zoom = Input.GetAxis(KeyMap.MapZoomOut, KeyMap.MapZoomIn);
+		if (zoom == 0) return;
 
-        Player.GlobalScale -= Vector2.One * zoom * ZoomSpeed * (float)delta;
-        Player.GlobalScale = Player.GlobalScale.Clamp(MinPlayerScale, MaxPlayerScale);
-    }
+		Player.GlobalScale -= Vector2.One * zoom * ZoomSpeed * (float)delta;
+		Player.GlobalScale = Player.GlobalScale.Clamp(MinPlayerScale, MaxPlayerScale);
+	}
 
-    public override void _Input(InputEvent @event)
-    {
-        if (Input.IsActionJustPressed(KeyMap.Map) && !Engine.IsEditorHint())
-        {
-            Visible = !Visible;
-        }
-    }
+	public override void _Input(InputEvent @event)
+	{
+		if (Input.IsActionJustPressed(KeyMap.Map) && !Engine.IsEditorHint())
+		{
+			Visible = !Visible;
+		}
+	}
 
-    private void UpdateMinimap()
-    {
-        MinimapContainer.Scale = Vector2.One * MiniMapScale / Player.GlobalScale;
-    }
+	private void UpdateMinimap()
+	{
+		MinimapContainer.Scale = Vector2.One * MiniMapScale / Player.GlobalScale;
+	}
+	
+	private void UpdatePlayerMarker()
+	{	
+		if (PlayerMarker == null || Player == null) return;
+		
+		Vector2 miniMapPosition = (Player.GlobalPosition / Player.GlobalScale) * MiniMapScale;
+		
+		PlayerMarker.GlobalPosition = MinimapContainer.GlobalPosition + miniMapPosition;
+		PlayerMarker.Scale = Vector2.One * MiniMapScale / Player.GlobalScale;
+	}
 }
